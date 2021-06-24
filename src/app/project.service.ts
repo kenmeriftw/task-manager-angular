8221e05cc1546f3ec6c1b9d731358a4963435785
Boris Parusnikov <2669997@gmail.com>
2021-06-24 22:50:39 +0300

    env added

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Project } from './project';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class ProjectService {
 
   constructor(private http: HttpClient) {}
 
-  private domain = "https://oblako-task-manager.herokuapp.com";
+  private domain = environment.apiUrl;
   private projects_url = this.domain + "/projects";
   private create_todo = this.domain + "/todos";
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiUrl: 'https://oblako-task-manager.herokuapp.com',
   production: true
 };
